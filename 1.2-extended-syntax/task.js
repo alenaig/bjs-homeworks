61bcfffa9a2248826a84ec7e7eccf98c8f4dd7b8
Alena Ignatyeva <ignatieva.alenka@gmail.com>
2020-08-03 21:54:38 +0300

    task 1 done

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,8 +1,18 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    
+    let d = Math.pow(b,2) - 4 * a * c;
+    let x = [];
+    if (d > 0) {
+        x[0] = (-b + Math.sqrt(d)) / (2 * a);
+        x[1] = (-b - Math.sqrt(d)) / (2 * a);
+    } else if (d == 0) {
+        x[0] = -b / (2 * a);
+    }
+      
+    return x;
 }
-
+/*
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
     // return averageMark;
@@ -12,3 +22,4 @@ function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
     // return result;
 }
+*/
