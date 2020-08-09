a19b8ae2a42b7196f6210ac1b9d3813cc6c19a1a
Alena Ignatyeva <ignatieva.alenka@gmail.com>
2020-08-09 23:21:37 +0300

    tasks without test

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,20 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+
+    //let arguments = [percent, contribution, amount, date];
+    /*for (argument of arguments) {
+        
+    } */
+    let credit = amount - contribution;
+    let month = date.getMonth() - new Date().getMonth() + 
+    (12 * (date.getFullYear() - new Date().getFullYear()));
+    let monthlyPercent = percent / 100 / 12;
+    let monthlyPayment = credit * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** month) - 1));
+    let totalAmount = month * monthlyPayment;
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    greeting = (name == null || name.length === 0 ? `Привет, мир! Меня зовут Аноним` : `Привет, мир! Меня зовут ${name}`);
+    return greeting;
 }
