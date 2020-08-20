9b856356540a6ea13aba38ada5d5a1ab62d9e5bd
Alena Ignatyeva <ignatieva.alenka@gmail.com>
2020-08-20 22:12:56 +0300

    2 and 3 tasks done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,29 @@
+ "use strict";
+
 //String.prototype.isPalindrome - для задачи №1
 
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+
+    let sum = 0;
+
+    if (marks.length == 0)
+        return 0;
+
+    for (let i = 0; i < marks.length; i++){
+        sum += marks[i];
+    }
+    
+    const average = sum / marks.length;
+    
+    return Math.round(average);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    
+    const now = Date.now();
+    const xday = +new Date(birthday); 
+    const diff = now - xday;
+    const age = diff / (((1000 * 60 * 60 * 24 * 365) * 3 + (1000 * 60 * 60 * 24 * 366)) / 4);
+    return age > 18 ? true : false;
 }
